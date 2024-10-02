23b7599c311fef9eaf6529f8b47a399a25cf8ea6
toktechteam <toktechteam@gmail.com>
2024-10-02 16:09:21 +0530

    Update settings.py
    Updated code for all ip

--- myhealthapp/settings.py
+++ myhealthapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-n@42coocz!(+wd0^l6p_-6u6%dthkak0_e3iy!%heamc5*0!1-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
